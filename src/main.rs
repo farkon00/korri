e36dedbb6e893bca4ad7a6dcc9ebf0b17c29d4bc
Robin Chapple <r.chapple.business@gmail.com>
2022-05-06 11:14:57 +0100

    Added file reading, and handling of file not found and other file i/o exceptions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 use std::env;
+use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>().drain(1..).collect();
-    if args.len() == 0 {
+    if args.is_empty() {
         println!("[ERROR] No arguments provided.");
         std::process::exit(1);
+    }
+    let filepath = &args[0];
+    debug_print_file(filepath);
+}
+
+fn file_error(error: ErrorKind, filepath: &str) {
+    match error {
+        ErrorKind::NotFound => {
+            println!("[ERROR] File '{}' not found.", filepath); 
+        },
+        _ => {
+            println!("[ERROR] File I/O error encountered: {:?}", error);
+        }
+    }
+
+    std::process::exit(1);
+}
+
+fn debug_print_file(filepath: &str) {
+    let contents = fs::read_to_string(filepath);
+    match contents {
+        Ok(_) => println!("File found and read successfully!"),
+        Err(e) => file_error(e.kind(), filepath),
     }
 }
