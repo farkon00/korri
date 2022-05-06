b974324b9a8d497f24a1bb49ead1029f7f630bf8
Robin Chapple <r.chapple.business@gmail.com>
2022-05-06 15:01:15 +0100

    Started iterating over the input file

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,20 @@
+
+// Allowing dead code as this is currently unimplemented
+#[allow(dead_code)]
+enum LexToken<'a> {
+    Keyword(&'a str),
+    BinaryOperation(&'a str),
+}
+
+fn lex_line(line: &str) {
+    println!("{}", line);
+}
+
+
+
 pub fn lex_file(file_contents: &str) {
-    println!("Lexing contents...\n{}", file_contents);
+    let lines: Vec<&str> = file_contents.split('\n').filter(|&x| !x.is_empty()).collect();
+    for line in &lines {
+        lex_line(line);
+    }
 }
